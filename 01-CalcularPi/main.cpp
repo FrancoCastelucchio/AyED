878fc9b6b2de8d4b9ae74c4f01ef7c6197583ea6
FrancoCastelucchio <134093317+FrancoCastelucchio@users.noreply.github.com>
2023-06-02 20:52:25 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+double calcularpi(int iteraciones) {
+    double pi = 0.0;
+    int signo = 1;
+
+    for (int i = 0; i < iteraciones; i++) {
+        double termino = 1.0 / (2 * i + 1);
+        pi += signo * termino;
+        signo *= -1;
+    }
+
+    return pi * 4;
+}
+
+int main() {
+    int numiteraciones = 1000000; 
+    double resultado = calcularpi(numiteraciones);
+    cout.precision(7); 
+
+    cout << "El valor aproximado de Pi es: " << resultado << endl;
+
+    return 0;
+}
